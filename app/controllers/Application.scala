a38a2001fa62c6965837b816c48fc1bf77d381d6
Nicolas Martignole <nicolas@martignole.net>
2012-08-04 15:48:27 +0200

    First form and license

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,39 @@
+/*
+ * Copyright (c) 2012. Nicolas Martignole
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package controllers
 
-import play.api._
+import play.api.data._
+import play.api.data.Forms._
 import play.api.mvc._
+import play.api.Play
+import play.api.data.validation.Constraints
+import play.api.Play.current
+
+case class RedisParameters(hostname: String, port: Long, auth: Option[String])
 
 object Application extends Controller {
-  
+
+  val paramsForm = Form(
+    mapping(
+      "hostname" -> text.verifying(Constraints.nonEmpty),
+      "port" -> longNumber,
+      "auth" -> optional(text)
+  )(RedisParameters.apply)(RedisParameters.unapply))
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    val hostname = Play.configuration.getString("redis.hostname").getOrElse("localhost")
+    val port = Play.configuration.getInt("redis.port").getOrElse(6379)
+    val auth = Play.configuration.getString("redis.auth")
+    Ok(views.html.index(paramsForm.fill(RedisParameters(hostname, port, auth))))
   }
-  
+
+  def startSession = Action{
+    TODO
+  }
 }
